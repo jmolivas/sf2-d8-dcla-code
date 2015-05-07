76e03fc5003fa01fecbcd24bdf377210519ba493
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-05-07 00:51:39 -0700

    Using Dependency Injection

--- src/AppBundle/ProductSerializer.php
+++ src/AppBundle/ProductSerializer.php
@@ -3,9 +3,15 @@
 namespace AppBundle;
 
 use AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class ProductSerializer
 {
+    private $router;
+
+    public function __construct(Router $router) {
+        $this->router = $router;
+    }
 
     public function serialize(Product $product)
     {
@@ -13,6 +19,7 @@ class ProductSerializer
           'id' => $product->getId(),
           'name' => $product->getName(),
           'price' => $product->getPrice(),
+          'url' => $this->router->generate('product_show', ['id' => $product->getId()])
         ];
 
         return $data;
